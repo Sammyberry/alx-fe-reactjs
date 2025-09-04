464dc5fc2bd908f3b8dd470f5ff8b6d91561dfdf
Sammyberry <samuel4weezy@yahoo.com>
2025-09-04 12:55:24 +0100

    Update

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,7 +1,19 @@
-import UserInfo from "./UserInfo";
+import { useContext } from "react";
+import UserContext from "./UserContext";
+
 
 function UserProfile() {
-  return <UserInfo />;
+  return (
+    <div>
+      <h2>User Profile</h2>
+      <p>
+        <strong>Name:</strong> {userData.name}
+      </p>
+      <p>
+        <strong>Email:</strong> {userData.email}
+      </p>
+    </div>
+  );
 }
 
 export default UserProfile;
