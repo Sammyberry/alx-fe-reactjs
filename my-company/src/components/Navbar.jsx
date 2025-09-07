24f91216c72ac19c5e704109d6ce476754873949
Sammyberry <samuel4weezy@yahoo.com>
2025-09-07 21:01:43 +0100

    Updated Version of My Company

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,14 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ background: "#333", padding: "10px" }}>
+    <nav
+      style={{
+        backgroundColor: "#333",
+        display: "flex",
+        justifyContent: "center",
+        padding: "10px",
+      }}
+    >
       <Link to="/" style={{ color: "white", margin: "0 10px" }}>
         Home
       </Link>
